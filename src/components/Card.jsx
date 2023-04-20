fd3debc21fcc9ac1211211618450a3b6d0237361
Samee Vohra <samee.vohra@gmail.com>
2023-04-20 11:38:47 -0700

    refactored card component to show flipped image

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,17 @@
 const Card = ({ image, selected, onClick }) => {
-
   return (
-    <div className="card">
+    <div className='card'>
       <div className={selected ? 'selected' : undefined}>
-        <img
-          alt=""
-          className="card-back"
+      <img alt='' src={image} className='card-face' />  
+      <img
+          alt=''
+          className='card-back'
           src={'/assets/fireship.png'}
           onClick={onClick}
         />
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Card;
