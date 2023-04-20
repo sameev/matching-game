009fdcf892595c1ea6ea988a4862c974e2f7ad3c
Samee Vohra <samee.vohra@gmail.com>
2023-04-19 17:42:02 -0700

    cleaned up index.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import './App.css';
+import '../App.css';
 
 function App() {
+  const [ cards, setCards ] = useState();
   return (
-    <div>
+    <div className='grid'>
+
 
     </div>
   );
